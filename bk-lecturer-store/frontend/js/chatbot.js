af98ac2ce74d40aa8768f57915472d2fff19f861
oanhnguyen06 <oanhnguyen20649@gmail.com>
2025-12-15 10:29:47 +0700

    Update chatbot.js

--- bk-lecturer-store/frontend/js/chatbot.js
+++ bk-lecturer-store/frontend/js/chatbot.js
@@ -1,1 +1,9 @@
+function send() {
+  const input = document.getElementById("msg");
+  const body = document.getElementById("chat-body");
+
+  body.innerHTML += `<div>${input.value}</div>`;
+  body.innerHTML += `<div class="bot">Mình sẽ hỗ trợ bạn sớm 😊</div>`;
+  input.value = "";
+}
 
