0c58c8b0e7cb0c6142063a639217f9c205d4d2ef
oanhnguyen06 <oanhnguyen20649@gmail.com>
2025-12-15 10:32:22 +0700

    Update crud.py

--- bk-lecturer-store/backend/app/crud.py
+++ bk-lecturer-store/backend/app/crud.py
@@ -1,1 +1,9 @@
+from sqlalchemy.orm import Session
+from . import models
+
+def get_all_lecturers(db: Session):
+    return db.query(models.Lecturer).all()
+
+def get_major_by_name(db: Session, name: str):
+    return db.query(models.Major).filter(models.Major.name.ilike(f"%{name}%")).first()
 
