3bec86d7ae755577940908cfb59f93056b2eee99
oanhnguyen06 <oanhnguyen20649@gmail.com>
2025-12-15 10:32:03 +0700

    Update schemas.py

--- bk-lecturer-store/backend/app/schemas.py
+++ bk-lecturer-store/backend/app/schemas.py
@@ -1,1 +1,17 @@
+from pydantic import BaseModel
+
+class LecturerBase(BaseModel):
+    name: str
+    title: str
+    role: str
+    unit: str
+    image: str
+
+class LecturerOut(LecturerBase):
+    id: int
+    class Config:
+        orm_mode = True
+
+class ChatRequest(BaseModel):
+    message: str
 
